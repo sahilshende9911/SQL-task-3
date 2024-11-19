d1a6151402b4de2a1b20615765a0d8a8a025e143
Sahil Ishwar Shende <156500067+sahilshende9911@users.noreply.github.com>
2024-11-20 00:17:22 +0530

    Add files via upload

---
+++ Scenerio 11 - Ranking Data - Task 3.sql
@@ -0,0 +1,14 @@
+--Scenario: Ranking Data
+
+--CREATE TABLE Participants (ParticipantID SERIAL PRIMARY KEY,ParticipantName VARCHAR(100) NOT NULL,
+--Score INT);
+
+--SELECT 
+    --ParticipantID,
+    --ParticipantName,
+    --Score,
+    --RANK() OVER (ORDER BY Score DESC) AS Rank
+--FROM Participants;
+
+
+
