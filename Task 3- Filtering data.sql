e9bb73b02f573b4f59a1684297cf94a8498e8c15
Sahil Ishwar Shende <156500067+sahilshende9911@users.noreply.github.com>
2024-12-11 02:19:59 +0530

    Add files via upload

---
+++ Task 3- Filtering data.sql
@@ -0,0 +1,4 @@
+-- create table employees (Employee_ID int primary key ,First_Name varchar,	
+-- LastName varchar,	Department varchar,	City varchar,	ManagerID int,	Salary int);
+
+-- select * from employees where department = 'IT' and salary > 50000;
