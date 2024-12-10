6171ed5a6af9705c9b35d9f77473653607f17d64
Sahil Ishwar Shende <156500067+sahilshende9911@users.noreply.github.com>
2024-12-11 02:18:42 +0530

    Add files via upload

---
+++ Task 3 - ranking data.sql
@@ -0,0 +1,3 @@
+-- create table participation (Participant_ID int, Participant_Name varchar,	Score int);
+
+-- select participant_name, score , rank () over (order by score desc ) as rank from participation; 
